2aa988e35609024964e651b737dce20c2acca935
lgou2w <lgou2w@hotmail.com>
2020-12-31 16:06:50 +0800

    feat: support stringify and fetch image

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
-import type { Gallery, Galleries } from './type'
+import type { Gallery, Galleries, Image, ImageType } from './type'
 import type { AxiosInstance, AxiosError } from 'axios'
+import type { Readable } from 'stream'
 import { httpsOverHttp, httpOverHttp } from 'tunnel'
 import { stringify as qs } from 'querystring'
 import axios from 'axios'
@@ -38,6 +39,15 @@ export class NHentaiAPI {
         debug('AXIOS -> %s %s', config.method!.toUpperCase(), config.url + param)
         return config
       })
+    }
+  }
+
+  static readonly DEFAULT_IMAGE_TYPE_TRANSFORM = (type: ImageType) => {
+    switch (type) {
+      case 'j': return 'jpg'
+      case 'p': return 'png'
+      case 'g': return 'gif'
+      default: throw new Error('不支持的图片类型转换：' + type)
     }
   }
 
@@ -87,5 +97,67 @@ export class NHentaiAPI {
       .get(`${URL.API}/galleries/tagged`, { params: { tag_id: tagId, page } })
       .then((res) => res.data)
       .catch(this.errorHandler)
+  }
+
+  stringifyImageUrl (
+    gallery: Gallery | number,
+    name: number | 'cover' | 'thumb',
+    image: Image | ImageType
+  ): string {
+    const mediaId = typeof gallery === 'object' ? parseInt(gallery.media_id) : gallery
+    if (typeof name === 'number' && name < 1) throw new Error('无效的图片名字索引，应大于 0 值')
+    if (isNaN(mediaId) || mediaId <= 0) throw new Error('无效的画廊媒体 ID 值：' + mediaId)
+    const url = typeof name === 'number' ? URL.IMAGE : URL.THUMB
+    const extension = NHentaiAPI.DEFAULT_IMAGE_TYPE_TRANSFORM(typeof image === 'object' ? image.t : image)
+    return `${url}/galleries/${mediaId}/${name}.${extension}`
+  }
+
+  stringifyImageUrls (
+    gallery: Gallery,
+    skipCoverAndThumbnail?: boolean
+  ): string[] {
+    skipCoverAndThumbnail = skipCoverAndThumbnail || false
+    const urls: string[] = []
+    if (!skipCoverAndThumbnail) {
+      urls.push(this.stringifyImageUrl(gallery, 'cover', gallery.images.cover))
+      urls.push(this.stringifyImageUrl(gallery, 'thumb', gallery.images.thumbnail))
+    }
+    const pages = gallery.images.pages
+    let i = 0
+    for (const page of pages) urls.push(this.stringifyImageUrl(gallery, ++i, page))
+    return urls
+  }
+
+  fetchImage (
+    gallery: Gallery | number,
+    name: number | 'cover' | 'thumb',
+    image: Image | ImageType
+  ): Promise<Readable> {
+    const url = this.stringifyImageUrl(gallery, name, image)
+    const host = (url.indexOf(URL.THUMB) !== -1 ? URL.THUMB : URL.IMAGE).substring(8) // https://
+    return this.req
+      .get<Readable>(url, { headers: { host }, responseType: 'stream' })
+      .then((res) => res.data)
+      .catch(this.errorHandler)
+  }
+
+  fetchImageAsBuffer (
+    gallery: Gallery | number,
+    name: number | 'cover' | 'thumb',
+    image: Image | ImageType
+  ): Promise<Buffer> {
+    const _this = this
+    // eslint-disable-next-line no-async-promise-executor
+    return new Promise<Buffer>(async (resolve, reject) => {
+      try {
+        const stream = await _this.fetchImage(gallery, name, image)
+        const buf: Buffer[] = []
+        stream.once('error', reject)
+        stream.on('data', (chunk: Buffer) => { buf.push(chunk) })
+        stream.on('end', () => { resolve(Buffer.concat(buf)) })
+      } catch (e) {
+        reject(e)
+      }
+    })
   }
 }
